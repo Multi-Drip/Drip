d693a5b2b4a551953374651c75f4f598f624850b
김예진 <wisl23428@gmail.com>
2022-06-18 17:43:16 +0900

    [김예진] test

--- dist/app.py
+++ dist/app.py
@@ -12,11 +12,11 @@ Data = pd.read_csv("DataL2.csv")
 Data_eng = pd.read_csv("Data_eng.csv")
 Data_jpn = pd.read_csv("Data_jpn.csv")
 Data = Data.dropna()
-Data_eng=Data_eng.dropna()
-Data_jpn=Data_jpn.dropna()
+Data_eng = Data_eng.dropna()
+Data_jpn = Data_jpn.dropna()
 cwd = os.getcwd()
 files = os.listdir(cwd)
-# 이런 주석을 임의로추가했다고 쳐봅시다
+# 김예진 오후5:42
 
 CORS(app)
 
@@ -33,91 +33,99 @@ LatitudeValue = Data['LatitudeValue']
 LongitudeValue = Data['LongitudeValue']
 SubImage = Data['SubImage']
 
-MainImage_eng=Data_eng['MainImage']
-Title_eng=Data_eng['Title']
-DataFilter_eng=Data_eng['DataFilter']
-PlaceFilter_eng=Data_eng['PlaceFilter']
-Subtitle_eng=Data_eng['Subtitle']
-DramaPlace_eng=Data_eng['DramaPlace']
-Dialogue_eng=Data_eng['Dialogue']
-Place_eng=Data_eng['Place']
-Address_eng=Data_eng['Address']
-LatitudeValue_eng=Data_eng['LatitudeValue']
-LongitudeValue_eng=Data_eng['LongitudeValue']
-SubImage_eng=Data_eng['SubImage']
+MainImage_eng = Data_eng['MainImage']
+Title_eng = Data_eng['Title']
+DataFilter_eng = Data_eng['DataFilter']
+PlaceFilter_eng = Data_eng['PlaceFilter']
+Subtitle_eng = Data_eng['Subtitle']
+DramaPlace_eng = Data_eng['DramaPlace']
+Dialogue_eng = Data_eng['Dialogue']
+Place_eng = Data_eng['Place']
+Address_eng = Data_eng['Address']
+LatitudeValue_eng = Data_eng['LatitudeValue']
+LongitudeValue_eng = Data_eng['LongitudeValue']
+SubImage_eng = Data_eng['SubImage']
 
-MainImage_jpn=Data_jpn['MainImage']
-Title_jpn=Data_jpn['Title']
-DataFilter_jpn=Data_jpn['DataFilter']
-PlaceFilter_jpn=Data_jpn['PlaceFilter']
-Subtitle_jpn=Data_jpn['Subtitle']
-DramaPlace_jpn=Data_jpn['DramaPlace']
-Dialogue_jpn=Data_jpn['Dialogue']
-Place_jpn=Data_jpn['Place']
-Address_jpn=Data_jpn['Address']
-LatitudeValue_jpn=Data_jpn['LatitudeValue']
-LongitudeValue_jpn=Data_jpn['LongitudeValue']
-SubImage_jpn=Data_jpn['SubImage']
+MainImage_jpn = Data_jpn['MainImage']
+Title_jpn = Data_jpn['Title']
+DataFilter_jpn = Data_jpn['DataFilter']
+PlaceFilter_jpn = Data_jpn['PlaceFilter']
+Subtitle_jpn = Data_jpn['Subtitle']
+DramaPlace_jpn = Data_jpn['DramaPlace']
+Dialogue_jpn = Data_jpn['Dialogue']
+Place_jpn = Data_jpn['Place']
+Address_jpn = Data_jpn['Address']
+LatitudeValue_jpn = Data_jpn['LatitudeValue']
+LongitudeValue_jpn = Data_jpn['LongitudeValue']
+SubImage_jpn = Data_jpn['SubImage']
 
-@app.route("/", methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def MainPage():
     return render_template('10PJ.html',
-                            len = len(Place),
-                            DataFilter = DataFilter,
-                            Place = Place,
-                            MainImage = MainImage,
-                            Title = Title,
-                            Subtitle = Subtitle)
+                           len=len(Place),
+                           DataFilter=DataFilter,
+                           Place=Place,
+                           MainImage=MainImage,
+                           Title=Title,
+                           Subtitle=Subtitle)
 
 
-@app.route("/engmain", methods=['GET','POST'])
+@app.route("/engmain", methods=['GET', 'POST'])
 def EngMain():
     return render_template('engmain.html',
-                            len = len(Place_eng),
-                            DataFilter_eng=DataFilter_eng,
-                            Place_eng=Place_eng,
-                            MainImage_eng=MainImage_eng,
-                            Title_eng=Title_eng,
-                            Subtitle_eng=Subtitle_eng)
+                           len=len(Place_eng),
+                           DataFilter_eng=DataFilter_eng,
+                           Place_eng=Place_eng,
+                           MainImage_eng=MainImage_eng,
+                           Title_eng=Title_eng,
+                           Subtitle_eng=Subtitle_eng)
 
-@app.route("/jpnmain", methods=['GET','POST'])
+
+@app.route("/jpnmain", methods=['GET', 'POST'])
 def JpnMain():
     return render_template('jpnmain.html',
-                            len = len(Place_jpn),
-                            DataFilter_jpn=DataFilter_jpn,
-                            Place_jpn=Place_jpn,
-                            MainImage_jpn=MainImage_jpn,
-                            Title_jpn=Title_jpn,
-                            Subtitle_jpn=Subtitle_jpn)
+                           len=len(Place_jpn),
+                           DataFilter_jpn=DataFilter_jpn,
+                           Place_jpn=Place_jpn,
+                           MainImage_jpn=MainImage_jpn,
+                           Title_jpn=Title_jpn,
+                           Subtitle_jpn=Subtitle_jpn)
 
-@app.route("/about", methods=['GET','POST'])
+
+@app.route("/about", methods=['GET', 'POST'])
 def About():
     return render_template('about.html')
 
-@app.route("/jpnabout", methods=['GET','POST'])
+
+@app.route("/jpnabout", methods=['GET', 'POST'])
 def JPNAbout():
     return render_template('jpnabout.html')
 
-@app.route("/engabout", methods=['GET','POST'])
+
+@app.route("/engabout", methods=['GET', 'POST'])
 def ENGAbout():
     return render_template('engabout.html')
- 
-@app.route("/coursejj", methods=['GET','POST'])
+
+
+@app.route("/coursejj", methods=['GET', 'POST'])
 def coursejj():
     return render_template('course01jj.html')
 
-@app.route("/coursegj", methods=['GET','POST'])
+
+@app.route("/coursegj", methods=['GET', 'POST'])
 def Coursegj():
     return render_template('course02gj.html')
 
-@app.route("/coursegw", methods=['GET','POST'])
+
+@app.route("/coursegw", methods=['GET', 'POST'])
 def Coursegw():
     return render_template('course03gw.html')
-    
-@app.route("/coursesu", methods=['GET','POST'])
+
+
+@app.route("/coursesu", methods=['GET', 'POST'])
 def Coursesu():
     return render_template('course04su.html')
-   
 
 
 @app.route("/locatesingle", methods=['GET', 'POST'])
@@ -125,44 +133,47 @@ def LocatePage():
     InputPlace = request.args.get('Place')
     IDX = np.where(Place == InputPlace)[0]
     return render_template('locatesingle.html',
-                            DramaPlace=DramaPlace[IDX].values[0],
-                            DataFilter=DataFilter[IDX],
-                            LatitudeValue=LatitudeValue[IDX].values[0],
-                            LongitudeValue=LongitudeValue[IDX].values[0],
-                            Dialogue=Markup(Dialogue[IDX].values[0]),
-                            Place=Place[IDX].values[0],
-                            Address=Address[IDX].values[0],
-                            SubImage=SubImage[IDX])
+                           DramaPlace=DramaPlace[IDX].values[0],
+                           DataFilter=DataFilter[IDX],
+                           LatitudeValue=LatitudeValue[IDX].values[0],
+                           LongitudeValue=LongitudeValue[IDX].values[0],
+                           Dialogue=Markup(Dialogue[IDX].values[0]),
+                           Place=Place[IDX].values[0],
+                           Address=Address[IDX].values[0],
+                           SubImage=SubImage[IDX])
 
-@app.route("/engsub", methods=['GET','POST'])
+
+@app.route("/engsub", methods=['GET', 'POST'])
 def EngSub():
     InputPlace_eng = request.args.get('Place')
     IDX = np.where(Place_eng == InputPlace_eng)[0]
     return render_template('engsub.html',
-                            DramaPlace_eng=DramaPlace_eng[IDX].values[0],
-                            DataFilter_eng=DataFilter_eng[IDX],
-                            LatitudeValue_eng=LatitudeValue_eng[IDX].values[0],
-                            LongitudeValue_eng=LongitudeValue_eng[IDX].values[0],
-                            Dialogue_eng=Markup(Dialogue_eng[IDX].values[0]),
-                            Place_eng=Place_eng[IDX].values[0],
-                            Address_eng=Address_eng[IDX].values[0],
-                            SubImage_eng=SubImage_eng[IDX])
+                           DramaPlace_eng=DramaPlace_eng[IDX].values[0],
+                           DataFilter_eng=DataFilter_eng[IDX],
+                           LatitudeValue_eng=LatitudeValue_eng[IDX].values[0],
+                           LongitudeValue_eng=LongitudeValue_eng[IDX].values[0],
+                           Dialogue_eng=Markup(Dialogue_eng[IDX].values[0]),
+                           Place_eng=Place_eng[IDX].values[0],
+                           Address_eng=Address_eng[IDX].values[0],
+                           SubImage_eng=SubImage_eng[IDX])
 
-@app.route("/jpngub", methods=['GET','POST'])
+
+@app.route("/jpngub", methods=['GET', 'POST'])
 def JpnSub():
     InputPlace_jpn = request.args.get('Place')
     IDX = np.where(Place_jpn == InputPlace_jpn)[0]
     return render_template('jpnsub.html',
-                            DramaPlace_jpn=DramaPlace_jpn[IDX].values[0],
-                            DataFilter_jpn=DataFilter_jpn[IDX],
-                            LatitudeValue_jpn=LatitudeValue_jpn[IDX].values[0],
-                            LongitudeValue_jpn=LongitudeValue_jpn[IDX].values[0],
-                            Dialogue_jpn=Markup(Dialogue_jpn[IDX].values[0]),
-                            Place_jpn=Place_jpn[IDX].values[0],
-                            Address_jpn=Address_jpn[IDX].values[0],
-                            SubImage_jpn=SubImage_jpn[IDX])
+                           DramaPlace_jpn=DramaPlace_jpn[IDX].values[0],
+                           DataFilter_jpn=DataFilter_jpn[IDX],
+                           LatitudeValue_jpn=LatitudeValue_jpn[IDX].values[0],
+                           LongitudeValue_jpn=LongitudeValue_jpn[IDX].values[0],
+                           Dialogue_jpn=Markup(Dialogue_jpn[IDX].values[0]),
+                           Place_jpn=Place_jpn[IDX].values[0],
+                           Address_jpn=Address_jpn[IDX].values[0],
+                           SubImage_jpn=SubImage_jpn[IDX])
 
-@app.route("/map", methods=['GET','POST'])
+
+@app.route("/map", methods=['GET', 'POST'])
 def AllMap():
     return render_template('map.html')
 
